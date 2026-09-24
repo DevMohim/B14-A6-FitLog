5fca8b0295224c82c2285e1c49a95d17028ca98f
Mohiuddin <mohiuddinn714@gmail.com>
2026-09-25 01:22:52 +0600

    added functionality for when user add upto 5 workout ,added todays plan button is disable and and a workout just 1 time

--- src/components/DetailsPage/AddPlanBtn.tsx
+++ src/components/DetailsPage/AddPlanBtn.tsx
@@ -6,17 +6,26 @@ import { LuCalendarPlus2 } from "react-icons/lu";
 import { toast } from "react-toastify";
 
 const AddPlanBtn = ({ workout }: { workout: IWorkoutType }) => {
-
   const { planCart, setPlanCart } = useWorkoutContext();
+  const isPlanCartFull = planCart.length >=5 ;
+  const isAlreadyAdded = planCart.some((plan) => plan.id === workout.id)
 
   const handleAddToPlan = () => {
+    if(isAlreadyAdded) {
+      toast.warn(`${workout.name} is already added`);
+      return
+    };
+
     setPlanCart([...planCart, workout]);
     toast.success(`${workout.name} Added to today's plan`);
   };
+
+
   return (
     <button
-      className="bg-green px-6 py-3 rounded-xl font-bold text-[12px] text-black tracking-[0.3px] cursor-pointer flex items-center gap-2"
+      className="bg-green px-6 py-3 rounded-xl font-bold text-[12px] text-black tracking-[0.3px] cursor-pointer flex items-center gap-2 disabled:cursor-not-allowed"
       onClick={() => handleAddToPlan()}
+      disabled={isPlanCartFull}
     >
       <LuCalendarPlus2 className="w-4 h-4" /> <span>Add to todays plan</span>
     </button>

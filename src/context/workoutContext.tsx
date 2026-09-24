b95f5b229803ad02d282647882f7554d92d63abf
Mohiuddin <mohiuddinn714@gmail.com>
2026-09-25 01:45:28 +0600

    added localstorage for servive plans  after page refresh

--- src/context/workoutContext.tsx
+++ src/context/workoutContext.tsx
@@ -1,24 +1,62 @@
-'use client'
+"use client";
+
 import { IWorkoutType } from "@/types/workout.type";
-import { createContext, Dispatch, useContext, useState, type ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+} from "react";
 
 interface CartContextType {
   planCart: IWorkoutType[];
-  setPlanCart: Dispatch<React.SetStateAction<IWorkoutType[]>>;
+  setPlanCart: Dispatch<SetStateAction<IWorkoutType[]>>;
   savedCart: IWorkoutType[];
-  setSavedCart: Dispatch<React.SetStateAction<IWorkoutType[]>>;
+  setSavedCart: Dispatch<SetStateAction<IWorkoutType[]>>;
 }
 
-export const workoutContext = createContext<CartContextType>({
-  planCart: [],
-  setPlanCart: () => {},
-  savedCart: [],
-  setSavedCart: () => {},
-});
+export const workoutContext = createContext<CartContextType | undefined>(
+  undefined,
+);
 
 const WorkoutProvider = ({ children }: { children: ReactNode }) => {
   const [planCart, setPlanCart] = useState<IWorkoutType[]>([]);
   const [savedCart, setSavedCart] = useState<IWorkoutType[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    try {
+      const storedPlanCart = localStorage.getItem("planCart");
+      const storedSavedCart = localStorage.getItem("savedCart");
+
+      if (storedPlanCart) {
+        setPlanCart(JSON.parse(storedPlanCart));
+      }
+
+      if (storedSavedCart) {
+        setSavedCart(JSON.parse(storedSavedCart));
+      }
+    } catch {
+      throw new Error("Could not load cart from localStorage:");
+    } finally {
+      setIsLoaded(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    localStorage.setItem("planCart", JSON.stringify(planCart));
+  }, [planCart, isLoaded]);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    localStorage.setItem("savedCart", JSON.stringify(savedCart));
+  }, [savedCart, isLoaded]);
 
   const cartObj: CartContextType = {
     planCart,
@@ -38,9 +76,10 @@ export const useWorkoutContext = () => {
   const context = useContext(workoutContext);
 
   if (!context) {
-    throw new Error("useAppContext must be used inside AppProvider");
+    throw new Error("useWorkoutContext must be used inside WorkoutProvider");
   }
 
   return context;
 };
+
 export default WorkoutProvider;

0932c421cd9095075c1d4505484b2abb352c67a9
Mohiuddin <mohiuddinn714@gmail.com>
2026-09-25 15:37:37 +0600

    modify error.tsx page for better  responsiveness

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ const notFound = () => {
       <div className="container mx-auto px-4 flex flex-col space-y-5 justify-center items-center min-h-screen">
         <Image src={NotFound} alt="Page not found" width={400} height={500} />
 
-        <h1 className="text-dark font-bold text-4xl tracking-wider">Opps! Page Not Found</h1>
+        <h1 className="text-dark font-bold text-3xl lg:text-4xl tracking-wider text-center">Opps! Page Not Found</h1>
 
         <Link href='/'>
           <button className="bg-green px-6 py-3 rounded-md font-bold text-[12px] text-black tracking-[0.3px] cursor-pointer">
